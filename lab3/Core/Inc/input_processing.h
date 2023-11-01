3233a1eb203cd7cc5731b451d5a03955cac0c199
ThanhDuy7 <thanh.daoeth3real07@hcmut.edu.vn>
2023-11-01 09:44:15 +0700

    initialize

---
+++ lab3/Core/Inc/input_processing.h
@@ -0,0 +1,13 @@
+/*
+ * input_processing.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: Thanh Duy
+ */
+
+#ifndef INC_INPUT_PROCESSING_H_
+#define INC_INPUT_PROCESSING_H_
+
+void fsm_for_input_processing(void);
+
+#endif /* INC_INPUT_PROCESSING_H_ */
---
+++ lab3/Core/Inc/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: Thanh Duy
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+
+
+#endif /* INC_TIMER_H_ */
